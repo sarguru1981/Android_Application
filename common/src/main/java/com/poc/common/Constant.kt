2522e2217caaccca2f0bfedbeaa0b1808969cc29
Sarguru <sarguru.subramanian@publicissapient.com>
2022-07-20 18:28:12 +0530

    Pushed common module changes

---
+++ common/src/main/java/com/poc/common/Constant.kt
@@ -0,0 +1,6 @@
+package com.poc.common
+
+object Constant {
+    const val APP_ID= "62c67c246c13fc4687712dda"
+    const val BASE_URL="https://dummyapi.io/data/v1/"
+}
---
+++ common/src/main/java/com/poc/common/Status.kt
@@ -0,0 +1,7 @@
+package com.poc.common
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
