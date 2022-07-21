0f0def1e80e26dd845374ddc37f9bc2e3ef44938
Sarguru <sarguru.subramanian@publicissapient.com>
2022-07-21 19:19:39 +0530

    Removed unncessary

--- data/src/main/java/com/poc/data/network/utils/Exception.kt
+++
@@ -1,7 +0,0 @@
-package com.poc.data.network.utils
-
-import java.io.IOException
-
-class ApiException(message: String) : IOException(message)
-
-class NoInternetException(message: String) : IOException(message)
